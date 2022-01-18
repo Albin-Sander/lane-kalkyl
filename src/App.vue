e88127d55dd12c97367fedd7dc21eae6e3294ef9
Albin Sander <51294735+Albin-Sander@users.noreply.github.com>
2022-01-18 18:18:59 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import LaneBelopp from "./components/LaneBelopp.vue";
+</script>
+
+<template>
+  <div class="flex justify-center w-full h-full">
+    <LaneBelopp class="flex content-center" />
+  </div>
+</template>
+
+<style></style>
---
+++ src/components/LaneBelopp.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex flex-row">
+    <p class="text-white pr-4">Lånebelopp</p>
+
+    <input
+      type="range"
+      min="0"
+      max="350000"
+      v-model="rangeValue"
+      placeholder="350 000 kr"
+      @change="changeValue"
+      class="range range-lg w-96 rounded-lg range-primary"
+    />
+    <p class="text-white pl-4">{{ Number(rangeValue).toLocaleString() }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "LaneBelopp",
+  data() {
+    return {
+      rangeValue: 0,
+    };
+  },
+  methods: {
+    changeValue: function () {
+      console.log(this.rangeValue);
+    },
+  },
+};
+</script>
+
+<style></style>
