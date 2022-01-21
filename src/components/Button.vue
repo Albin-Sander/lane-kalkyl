d4496180a4e772de50a20329b6962d18d1a9450c
Albin Sander <51294735+Albin-Sander@users.noreply.github.com>
2022-01-22 00:53:54 +0100

    Added support for dynamic routes

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,13 +1,34 @@
 <template>
-  <button class="btn btn-primary bg-green-500 hover:bg-green-600">
+  <button
+    class="btn btn-primary bg-green-500 hover:bg-green-600"
+    v-on:click="addRoute"
+  >
     Till ansökan
   </button>
 </template>
 
 <script lang="ts">
-export default {
-  name: "Button",
-};
+import { defineComponent } from "vue";
+import router from "../router";
+import LoanApplication from "../views/LoanApplication.vue";
+
+// /loan-application/?amount=100000&months=120
+
+const Button = defineComponent({
+  methods: {
+    addRoute: async function () {
+      let path = `?amount=${this.$store.state.lanebelopp}&months=${this.$store.state.lanetid}`;
+      console.log(router.getRoutes());
+      router.addRoute("LoanApplication", {
+        path: path,
+        component: LoanApplication,
+      });
+      await router.replace(`/loan-application/${path}`);
+    },
+  },
+});
+
+export default Button;
 </script>
 
 <style></style>
